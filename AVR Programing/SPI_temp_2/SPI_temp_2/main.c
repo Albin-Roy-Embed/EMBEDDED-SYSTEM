e7aa90e401d04a20c31a985169ff965b55aa0bca
ALBIN ROY <albinroy800@gmail.com>
2026-01-22 23:45:26 +0530

    Initial commit for STM32 driver development(WIP)

---
+++ AVR Programing/SPI_temp_2/SPI_temp_2/main.c
@@ -0,0 +1,18 @@
+/*
+ * SPI_temp_2.c
+ *
+ * Created: 13/01/2026 21:03:19
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
