909dbcd83c388b559102dbf4b954d6997d3d3293
Albin Roy <albinroy800@gmail.com>
2026-05-05 21:05:08 +0530

    Added Some Changes

---
+++ AVR Programing/AVR Q&A/Toggle_LED/Toggle_LED/main.c
@@ -0,0 +1,30 @@
+/*
+ * Toggle_LED.c
+ *
+ * Created: 05/05/2026 21:02:00
+ * Author : admin
+ */ 
+
+
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+
+
+int main(void)
+{
+	DDRD|=(1<<PD5);
+	
+	
+	while (1)
+	{
+		PORTD|=(1<<PD5);
+		_delay_ms(5000);
+		PORTD&=~(1<<PD5);
+	}
+	
+	return 0;
+}
+
+
