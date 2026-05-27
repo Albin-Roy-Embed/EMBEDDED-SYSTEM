58e708ea79b02f09a24d8600bf70401b0a64d5be
Albin Roy <albinroy800@gmail.com>
2026-05-27 20:03:04 +0530

    New commit

---
+++ AVR Programing/AVR Q&A/Led_Train/main.c
@@ -0,0 +1,26 @@
+/*
+ * Led_Train.c
+ *
+ * Created: 18/05/2026 20:24:02
+ * Author : admin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+   DDRB |= (1<<PB0) | (1<<PB1) | (1<<PB2) | (1<<PB3) |
+   (1<<PB4) | (1<<PB5) | (1<<PB6) | (1<<PB7);
+   
+   int led_one
+	   
+   }
+   
+	
+    while (1) 
+    {
+		
+    }
+}
+
